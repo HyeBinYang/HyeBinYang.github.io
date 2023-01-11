6ee13e018db0ee6c8f13723a8946cea8b0cb8f5c
HyeBinYang <skdisk7368@gmail.com>
2023-01-12 00:49:45 +0900

    feat : 댓글 기능 추가

---
+++ src/components/Utterances.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef } from "react";
+
+const Utterances = () => {
+  const commentEl = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scriptEl = document.createElement("script");
+    scriptEl.async = true;
+    scriptEl.src = "https://utteranc.es/client.js";
+    scriptEl.setAttribute("repo", "HyeBinYang/blog");
+    scriptEl.setAttribute("issue-term", "pathname");
+    scriptEl.setAttribute("theme", "github-light");
+    scriptEl.setAttribute("crossorigin", "anonymous");
+
+    commentEl.current?.appendChild(scriptEl);
+  }, []);
+
+  return <div ref={commentEl} />;
+};
+
+export default Utterances;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Global } from "@emotion/react";
 import { globalStyle } from "../style";
 import Header from "components/Header";
+import Utterances from "components/Utterances";
 
 const Home = () => {
   return (
     <div>
       <Global styles={globalStyle} />
       <Header />
+      <Utterances />
     </div>
   );
 };
